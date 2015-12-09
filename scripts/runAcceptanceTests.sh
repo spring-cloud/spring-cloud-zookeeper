4efe2414a5139c95349b26710b3160d6f2d48e1b
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2015-12-09 23:58:00 +0100

    Fixed url to acceptance tests

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-curl https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/scripts/runDockerAcceptanceTests.sh --output runDockerAcceptanceTests.sh
+curl https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh --output runDockerAcceptanceTests.sh
 sh runDockerAcceptanceTests.sh -DWHAT_TO_TEST=SERVICE_REGISTRY
