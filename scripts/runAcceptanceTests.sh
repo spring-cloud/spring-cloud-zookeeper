90cf150acca1439c5d0a6736a98308b2461dbfa6
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2015-12-10 11:50:55 +0100

    Updated acceptance tests scripts args passing

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -2,4 +2,4 @@
 set -e
 
 curl https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh --output runDockerAcceptanceTests.sh
-sh runDockerAcceptanceTests.sh -DWHAT_TO_TEST=SERVICE_REGISTRY
+sh runDockerAcceptanceTests.sh -t=SERVICE_REGISTRY -v=1.0.0.BUILD-SNAPSHOT
