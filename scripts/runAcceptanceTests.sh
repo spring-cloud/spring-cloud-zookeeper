6fe8ee48ac04bb7fc5ccd50a93f96eac6cbe788a
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2015-12-14 15:53:53 +0100

    Fixed what to test

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
-AT_WHAT_TO_TEST="SERVICE_REGISTRY"
+AT_WHAT_TO_TEST="ZOOKEEPER"
 AT_VERSION="1.0.0.BUILD-SNAPSHOT"
 
 curl "${SCRIPT_URL}" --output runDockerAcceptanceTests.sh
