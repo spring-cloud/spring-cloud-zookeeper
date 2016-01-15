7cc3daa6b5a029a398ea21d66b08ede58d833940
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-01-15 12:38:49 +0100

    Updated e2e scripts

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -2,12 +2,11 @@
 
 set -o errexit
 
-SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/runAcceptanceTests.sh"
 AT_WHAT_TO_TEST="ZOOKEEPER"
 
-curl "${SCRIPT_URL}" --output runDockerAcceptanceTests.sh
+curl "${SCRIPT_URL}" --output runAcceptanceTests.sh
 
-chmod +x runDockerAcceptanceTests.sh
+chmod +x runAcceptanceTests.sh
 
-# Run the tests and clean up
-./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -k 1
+./runAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -k 1
