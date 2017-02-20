057b52545b851a0496337449cd5ef5ff45f03f10
Spencer Gibb <spencer@gibb.us>
2017-02-20 14:02:28 -0700

    Make spring-boot-starter-web truly optional.
    Fixes gh-103

--- spring-cloud-zookeeper-sample/src/test/java/org/springframework/cloud/zookeeper/sample/SampleApplicationTests.java
+++ spring-cloud-zookeeper-sample/src/test/java/org/springframework/cloud/zookeeper/sample/SampleApplicationTests.java
@@ -19,16 +19,28 @@ package org.springframework.cloud.zookeeper.sample;
 import org.apache.curator.test.TestingServer;
 import org.junit.Test;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.SocketUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SampleApplicationTests {
 
 	@Test public void contextLoads() throws Exception {
-		int port = SocketUtils.findAvailableTcpPort();
-		TestingServer server = new TestingServer(port);
+		int zkPort = SocketUtils.findAvailableTcpPort();
+		TestingServer server = new TestingServer(zkPort);
 
-		ConfigurableApplicationContext context = new SpringApplicationBuilder(SampleZookeeperApplication.class).run("--server.port=0", "--spring.cloud.zookeeper.connectString=localhost:" + port);
+		int port = SocketUtils.findAvailableTcpPort(zkPort+1);
+
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(SampleZookeeperApplication.class).run(
+				"--server.port="+port,
+				"--spring.cloud.zookeeper.connectString=localhost:" + zkPort);
+
+		ResponseEntity<String> response = new TestRestTemplate().getForEntity("http://localhost:"+port+"/health", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
 		context.close();
 		server.close();
