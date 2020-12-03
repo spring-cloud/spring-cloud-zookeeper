067615be1e41438a1a5ac7a6a716c4507c5ae951
Michael Sievers <msievers@wescale.com>
2020-12-03 18:11:04 +0100

    Add test to ensure correct property source order of composite property source
    * spring-cloud/spring-cloud-zookeeper#270

--- spring-cloud-zookeeper-config/src/test/java/org/springframework/cloud/zookeeper/config/ZookeeperPropertySourceLocatorTests.java
+++ spring-cloud-zookeeper-config/src/test/java/org/springframework/cloud/zookeeper/config/ZookeeperPropertySourceLocatorTests.java
@@ -16,6 +16,11 @@
 
 package org.springframework.cloud.zookeeper.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
@@ -25,12 +30,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.GetChildrenBuilder;
 import org.apache.curator.retry.RetryOneTime;
 import org.apache.zookeeper.KeeperException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -43,9 +48,10 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.core.env.PropertySource;
+import org.springframework.mock.env.MockEnvironment;
 
 /**
  * @author Spencer Gibb
@@ -185,6 +191,45 @@ public class ZookeeperPropertySourceLocatorTests {
 		testProp = this.environment.getProperty(KEY_BASIC);
 		assertThat(testProp).as("testProp was wrong after update")
 				.isEqualTo("testPropValUpdate");
+	}
+
+	@Test
+	public void compositePropertySourceHoldsPropertySourcesInCorrectOrder() {
+
+		// given
+		final String defaultContext = "someDefaultContext";
+		final String someName = "someName";
+		final String someProfile = "someProfile";
+
+		final CuratorFramework curator = mock(CuratorFramework.class);
+		when(curator.getChildren()).thenReturn(mock(GetChildrenBuilder.class));
+
+		final MockEnvironment mockEnvironment = new MockEnvironment();
+		mockEnvironment.setActiveProfiles(someProfile);
+
+		final ZookeeperConfigProperties properties = new ZookeeperConfigProperties();
+		properties.setName(someName);
+		properties.setDefaultContext(defaultContext);
+
+		final ZookeeperPropertySourceLocator locator = new ZookeeperPropertySourceLocator(
+				curator, properties);
+
+		// when
+		final PropertySource<?> propertySource = locator.locate(mockEnvironment);
+
+		// then
+		assertThat(propertySource).isInstanceOf(CompositePropertySource.class);
+
+		// and
+		final ArrayList<PropertySource<?>> propertySources = new ArrayList<>(
+				((CompositePropertySource) propertySource).getPropertySources());
+
+		assertThat(propertySources.get(0).getName())
+				.endsWith(someName + properties.getProfileSeparator() + someProfile);
+		assertThat(propertySources.get(1).getName()).endsWith(someName);
+		assertThat(propertySources.get(2).getName()).endsWith(
+				defaultContext + properties.getProfileSeparator() + someProfile);
+		assertThat(propertySources.get(3).getName()).endsWith(defaultContext);
 	}
 
 	@Configuration
