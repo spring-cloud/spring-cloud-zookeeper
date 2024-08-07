a39bbab6bbeaecaed2d1136931735fcf5650f0c2
Olga Maciaszek-Sharma <omaciaszeksh@vmware.com>
2024-08-07 20:05:57 +0200

    Fix DiscoveryDisabledTests.

--- spring-cloud-zookeeper-discovery/src/test/java/org/springframework/cloud/zookeeper/discovery/ZookeeperDiscoveryDisabledTests.java
+++ spring-cloud-zookeeper-discovery/src/test/java/org/springframework/cloud/zookeeper/discovery/ZookeeperDiscoveryDisabledTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 the original author or authors.
+ * Copyright 2015-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.mockito.Mockito.mock;
+
 /**
  * @author Marcin Grzejszczak
  */
@@ -52,7 +54,7 @@ public class ZookeeperDiscoveryDisabledTests {
 
 		@Bean
 		CuratorFramework curator() {
-			return null;
+			return mock(CuratorFramework.class);
 		}
 
 	}
